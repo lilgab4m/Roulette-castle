b29f52dd1165bae02108cf2d1c224bdf54b5fdb0
lilgab4m <130669520+lilgab4m@users.noreply.github.com>
2024-04-04 09:26:35 -0300

    d
    d

---
+++ rodape/rodape.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rodapé Estilizado</title>
+    <link rel="stylesheet" href="../rodape/rodape.css   ">
+</head>
+<body>
+  
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-section">
+                <h3>Sobre Nós</h3>
+                <p>Somos um site que ofereçemos diverçao e a oportunidade de ganhar dinheiro .</p>
+            </div>
+            <div class="footer-section">
+                <h3>Links Rápidos</h3>
+                <ul>
+                    <li><a href="#">Página Inicial</a></li>
+                    <li><a href="#">Sobre</a></li>
+            
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+            <div class="footer-section">
+                <h3>Contato</h3>
+                 Telefone: (+1) 555-1234<br> Email: roulette.castle@Email.com</p>
+            </div>
+        </div>
+        <div class="bottom-bar">
+            <p>&copy; 2024 - Todos os direitos reservados</p>
+            <p>Desenvolvido por <a href="#" target="_blank">G2</a></p>
+        </div>
+    </footer>''
+</body>
+</html>
