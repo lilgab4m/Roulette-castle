de54c3478eb7a2a5f0fb54f989c38626961d1bce
CAmillymilly <130669887+CAmillymilly@users.noreply.github.com>
2024-04-12 09:51:16 -0300

    commit da camilly

---
+++ sobre/sobre.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
+    <style>
+        footer {
+            background-color: #282828;
+            color: #fff;
+            padding: 5px 0;
+            font-size: 14px;
+            text-align: center;
+            bottom: 0;
+            width: 100%;
+        }
+        .footer-content {
+            max-width: 1222;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+        .footer-section {
+            margin-bottom: 20px;
+        }
+        .footer-section h3 {
+            color: #10BB01;
+            margin-bottom: 10px;
+        }
+        .footer-section ul {
+            list-style-type: none;
+            padding: 0;
+        }
+   
+        .footer-section ul li a {
+            color: #fff;
+            text-decoration: none;
+            transition: color 0.3s ease;
+        }
+        .footer-section ul li a:hover {
+            color: #10BB01;
+        }
+        .bottom-bar {
+            background-color: #383838;
+            padding: 10px 0;
+            text-align: center;
+            width: 100%;
+        }
+        .bottom-bar p {
+            margin: 5px 0;
+        }
+        .bottom-bar a {
+            color: #10BB01;
+            text-decoration: none;
+            transition: color 0.3s ease;
+        }
+        .bottom-bar a:hover {
+            text-decoration: underline;
+        }
+        .container{
+            font-family: "Questrial", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }  
+        h1{
+            font-family: "Righteous", sans-serif;
+            font-weight: 400;
+            font-style: h1;
+            color: #10BB01;
+        }
+        body {
+        background-color: #282828;
+        color: #fff;
+        }
+        .container {
+            width: 48%; /* Define a largura do contêiner */
+            margin: 0 auto; /* Centraliza o contêiner na página */
+            padding: 40 px; /* Adiciona preenchimento interno ao contêiner */
+        }
+
+    </style>
+</head>
+
+<div class="container">
+        <center><h1>SOBRE NÓS</h1></center>
+        Somos a Roulette Castle, uma empresa de jogos online apaixonada por criar experiências incríveis para os jogadores de todo o mundo. Nossa missão é proporcionar diversão, entretenimento e desafios emocionantes por meio dos nossos <center>jogos inovadores.</center>  
+        </p>
+        Com o compromisso de consolidar nossa presença nos meios digitais, estamos empenhados em oferecer títulos cativantes que transcendem fronteiras e culturas. Nosso objetivo é alcançar sucesso financeiro enquanto mantemos a qualidade e a <center>originalidade que nos tornam únicos.</center>
+        </p>
+        Na Roulette Castle, acreditamos que os jogos podem unir pessoas e criar comunidades vibrantes. Por isso, buscamos aumentar nossa visibilidade em outros países, levando nossos jogos para novos públicos e compartilhando a paixão pelo <center>entretenimento interativo.</center>
+        </p>
+        Junte-se a nós nesta jornada emocionante e explore o mundo dos jogos online de uma maneira totalmente nova. Descubra aventuras, estratégias e diversão sem limites em cada um dos nossos títulos. Seja bem-vindo à Roulette Castle, onde a <center>magia dos jogos ganha vida!</center>
+        </p>
+    </div>
+<body>
+<footer>
+    <div class="footer-content">
+        <div class="bottom-bar">
+            <p>&copy; 2024 - Todos os direitos reservados</p>
+            <p>Desenvolvido por <a href="#" target="_blank">G2</a></p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
